a3a019fb7a3be448371a811520f674e6e10c3bbc
Aaron Scofield <tuj83098@temple.edu>
2022-02-24 21:27:51 -0500

    created navbar component, updated app css, imported component into app.js

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './App.style';
+import Navbar from './components/navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <div className="App">
+        <Navbar />
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </div>
     </View>
   );
 }
---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
+const Navbar = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>Navbar</Toolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar;
