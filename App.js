6e82592d7dec8bc5230bcd3590b86a7d901c7394
Aaron Scofield <tuj83098@temple.edu>
2022-02-24 23:43:17 -0500

    created navbar component, no links yet since there are no other components

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './App.style';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar.js'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <div className="App">
         <Navbar/>
-        <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </div>
     </View>
--- components/navbar.js
+++ components/navbar.js
@@ -9,8 +9,11 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
+import Container from '@mui/material/Container';
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
 
-const pages = ['About', 'Record', 'Calendar'];
+const pages = ['Calendar', 'Record', 'About'];
 
 const appTheme = createTheme({
     palette: {
@@ -21,35 +24,82 @@ const appTheme = createTheme({
   });
 
 export default function Navbar() {
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    /*const changePage = (pageName) => {
+        console.log("swap to: " + pageName.toString());
+    }*/
+
     return (
         <ThemeProvider theme={appTheme}>
-            <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
-                <Toolbar>
-                    <IconButton 
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{mr: 2}}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <AccessTimeIcon
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="clock-icon"
-                        sx={{mr: .5}}
-                    />
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        60 Seconds
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <AccessTimeIcon sx={{ mr: 1 }}/>
+                        <Typography variant="h6" noWrap component="div" sx={{mr: 2, display: { xs: 'none', md: 'flex' }}}>
+                            60 Seconds
+                        </Typography>
+                            
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton 
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                onClick={handleOpenNavMenu}
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal:'left'
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: {xs: 'block', md: 'none'}
+                                }}
+                            >
+                                {pages.map((page) => (
+                                    <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                        <Typography textAlign="center">{page}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </Box>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                            ))}
+                        </Box>  
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </Container>
             </AppBar>
-        </Box>
         </ThemeProvider>
-        
     )
 }
