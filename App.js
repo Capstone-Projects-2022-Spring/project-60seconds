6d9ad51e6ac7e5eb2f1c83f6cb009dc476e3be32
Xu Lyu <tuj85073@temple.edu>
2022-03-07 14:53:25 -0500

    Login component

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import styles from './App.style';
 import Navbar from './components/Navbar.js'
+import Login from './components/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <div className="App">
         <Navbar/>
+        <Login />
         <StatusBar style="auto" />
       </div>
     </View>
