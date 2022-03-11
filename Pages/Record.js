df8edf9bd4431d6125f27e173d392883bf047e0c
Ryan <rhardison455@outlook.com>
2022-03-11 15:14:06 -0500

    audio recording sorta working

--- Pages/Record.js
+++ Pages/Record.js
@@ -1,15 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import styles from '../App.style';
-import Recorder from '../components/recorder'
+import { Audio } from 'expo-av';
+import * as React from 'react';
+import { StyleSheet, Button } from 'react-native';
+
 
 export default function Record() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    const mp3 = recording.mp3();
+    console.log('Recording stopped and stored at', uri, mp3);
+  }
+
   return (
-    <View style={styles.container}>
-      <div className="App">
-        <Recorder/>
-        <StatusBar style="auto" />
-      </div>
+    <View style={recordStyles.container}>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
     </View>
   );
 }
+
+const recordStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 10,
+  },
+});
