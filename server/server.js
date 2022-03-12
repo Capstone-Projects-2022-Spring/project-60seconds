564ae49e7aaa33e4de6eebca0f47453c9bb909e3
Zack Waxler <tuj00149@temple.edu>
2022-03-12 15:07:52 -0800

    Create account functionality complete

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 // server.js
 // Main file; handles incoming API requests to the server
 
-// TODO: Needs a lot of refactoring
+// TODO: Needs a lot of refactoring; split logic between multiple files, fix vulnerabilities, clean things up
 
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const AWS = require('aws-sdk');
 const fs = require('fs');
 
 const creds = require('./creds');
 
-const dbConnection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: 'sixtyseconds.cvdaqyvuh6xj.us-east-1.rds.amazonaws.com', // host for connection
   port: 3306, // default port for mysql is 3306
   database: 'main', // database from which we want to connect out node application
@@ -18,22 +18,42 @@ const dbConnection = mysql.createConnection({
 });
 
 // TODO: Fix glaring SQL injection vulnerabilities
-let checkCredentials = (dbConnection, username, password) => {
-  let sql = `SELECT * FROM users WHERE username LIKE '${username}' AND password LIKE '${password}'`;
+// let checkCredentials = (username, password) => {
+//
+//
+//   let sql = `SELECT * FROM users WHERE username LIKE '${username}' AND password LIKE '${password}'`;
+//   let check = false;
+//
+//   connection.query(sql, function (err, result, fields) {
+//     if (err) throw err;
+//
+//     let parsedResult = JSON.parse(JSON.stringify(result))[0];
+//     console.log(result.length);
+//
+//     if (result.length === 1) {
+//       check = true;
+//     }
+//   });
+//
+//   return check;
+// }
 
-  dbConnection.query(sql, function (err, result, fields) {
-    if (err) throw err;
-    let parsedResult = JSON.parse(JSON.stringify(result))[0];
-
-    console.log(parsedResult);
-
-    if (parsedResult === undefined) {
-      return false;
-    }
-
-    return true;
-  });
-}
+// let checkAccountExists = (dbConnection, username) => {
+//   let sql = `SELECT * FROM users WHERE username LIKE '${username}'`;
+//
+//   dbConnection.query(sql, function (err, result, fields) {
+//     if (err) throw err;
+//     let parsedResult = JSON.parse(JSON.stringify(result))[0];
+//
+//     console.log(parsedResult);
+//
+//     if (parsedResult === undefined) {
+//       return false;
+//     }
+//
+//     return true;
+//   });
+// }
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -48,15 +68,62 @@ app.get(app.prefix, (req, res) => {
 });
 
 // Handle login attempts
-app.post('/api/login', (req, res) => {
-  console.log(req.body);
-  if (checkCredentials(dbConnection, req.body.username, req.body.password)) {
-    res.status(200)
-      .end(req.body.username);
-  } else {
-    res.status(401)
-      .end();
-  }
+app.post('/api/login', async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let sql = `SELECT * FROM users WHERE username LIKE '${username}' AND password LIKE '${password}'`;
+
+    connection.query(sql, function (err, result, fields) {
+      if (err) throw err;
+
+      let parsedResult = JSON.parse(JSON.stringify(result))[0];
+
+      if (result.length === 1) {
+          console.log('[/api/login] Succesful login: ' + username + ':' + password)
+          res.status(200)
+            .end(req.body.username);
+      } else {
+          console.log('[/api/login] Unsuccesful login attempt: ' + req.body.username + ':' + req.body.password);
+          res.status(401)
+            .end();
+      }
+    });
+});
+
+app.post('/api/create_account', (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  let statement = `SELECT * FROM users WHERE username LIKE '${username}'`;
+  connection.query(statement, function (err, result, fields) {
+    if (err) throw err;
+
+    let parsedResult = JSON.parse(JSON.stringify(result))[0];
+
+    // Account already exists, return 400 error
+    if (result.length === 1) {
+        console.log('[/api/create_account] Error: user already exists ' + username + ':' + password);
+
+
+        res.status(400)
+          .end(req.body.username);
+
+    // Create a new account
+    } else {
+        console.log('[/api/create_account] Creating new account ' + username + ':' + password);
+
+        let statement = `INSERT INTO users (username, password) VALUES ('${username}', '${password}')`;
+        console.log('Running statement ' + statement);
+        connection.query(statement, function (err, result, fields) {
+          // User created, return 200 OK
+          console.log('Query executed');
+
+          res.status(200)
+            .end();
+        });
+    }
+  });
 });
 
 const port = 8080; // TODO: change for production
