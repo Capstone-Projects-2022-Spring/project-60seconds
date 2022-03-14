f52c8039f5875d742810c3e3717992df3cd2f91e
Ryan Hardison <rhardison455@outlook.com>
2022-03-14 00:10:08 -0400

    recording and playback functional, issues with displaying recording duration and getting POST requests

--- components/recorder.js
+++ components/recorder.js
@@ -1,15 +1,16 @@
-import { Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Audio } from 'expo-av';
 import * as React from 'react';
-import { Button } from 'react-native';
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { Recording } from 'expo-av/build/Audio';
+import { StatusBar } from 'expo-status-bar';
 
 
 export default function recorder() {
 
 	const [recording, setRecording] = React.useState();
+	const [recordings, setRecordings] = React.useState([]);
 
 	async function startRecording() {
 		try {
@@ -34,29 +35,70 @@ export default function recorder() {
 		console.log('Stopping recording..');
 		setRecording(undefined);
 		await recording.stopAndUnloadAsync();
-		const localUri = recording.getURI(); 
-		
-		console.log('Recording stopped and stored at', localUri);
+		const { sound, status } = await recording.createNewLoadedSoundAsync();
+		console.log("recording length: " + status.durationMillis);
 
-		/*
+		let updatedRecordings = [...recordings];
+		
+		updatedRecordings.push({
+			sound: sound,
+			duration: getDurationFormatted(status.durationMillis),
+			file: recording.getURI()
+		});
+
+		setRecordings(updatedRecordings);
+
+		
 		let formRequest = new FormData();
 		formRequest.append('audio', {
-			uri: localUri,
+			uri: recording.getURI,
 			name: 'file.mp3',
 			type: 'audio/mp3'
 		})
-		formRequest.append('username', 'Aaron');
+		formRequest.append('username', 'testRegister');
 
 		return await fetch('http://54.226.36.70/api/upload', {
 			method: 'POST',
 			body: formRequest,
-			headers: {
-			  'content-type': 'multipart/form-data',
-			},
-		  }).catch((error) => {
+			
+		  }).then(data => console.log(data)).then(data => console.log(data))
+		  .catch((error) => {
 			  console.log(error);
 		  });
-		*/
+
+		  /*axios.post('http://54.226.36.70/api/upload', {
+			uri: recording.getURI,
+			name: 'file.mp3',
+			type: 'audio/mp3'
+		  })
+		  .then(function (response) {
+			console.log(response);
+		  })
+		  .catch(function (error) {
+			console.log(error);
+		  });*/
+
+
+	}
+
+	function getDurationFormatted(millis) {
+		console.log("Millis is: " + millis);
+		const minutes = millis / 1000 / 60;
+		const minutesDisplay = Math.floor(minutes);
+		const seconds = Math.round((minutes - minutesDisplay) * 60);
+		const secondsDisplay = seconds < 10 ? `0${seconds}` : seconds;
+		return `${minutesDisplay}:${secondsDisplay}`;
+	}
+
+	function getRecordingLines() {
+		return recordings.map((recordingLine, index) => {
+			return (
+				<View key={index} style={StyleSheet.row}>
+					<Text style={styles.fill}>Recording {index+1} - {recordingLine.duration}</Text>
+					<Button style={styles.button} onPress={() => recordingLine.sound.replayAsync()} title="Play"></Button>
+				</View>
+			);
+		});
 	}
 
 	return (
@@ -76,8 +118,30 @@ export default function recorder() {
 					variant="contained"
 					sx={{ mt: 3, mb: 2 }}
 				/>
+				{getRecordingLines()}
 			</Box>
 		</Container>
 		
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	row: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	fill: {
+		flex: 1,
+		margin: 1,
+	},
+	button: {
+		margin: 16,
+	}
+});
