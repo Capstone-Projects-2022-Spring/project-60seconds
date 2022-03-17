fb7ac976b1b1f8dd68baf1739f6ff01f146f629f
Aaron Scofield <tuj83098@temple.edu>
2022-03-17 18:19:12 -0400

    Calendar.js loads in CalendarComponent.js

--- Pages/Calendar.js
+++ Pages/Calendar.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
-import styles from '../App.style';
+import styles from '../components/calendar.css';
+import CalendarComponent from '../components/CalendarComponent';
 
 export default function Calendar() {
   return (
     <View style={styles.container}>
       <div className="App">
-        <p>Calendar Page</p>
+        <CalendarComponent/>
         <StatusBar style="auto" />
       </div>
     </View>
