48129611d8dc4ff974efd2986d95ec72e18fdbb0
Aaron Scofield <tuj83098@temple.edu>
2022-03-21 14:12:38 -0400

    fixed accidental background color change

--- App.style.js
+++ App.style.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#000000',
+      backgroundColor: '#ffffff',
       flexDirection: 'row'
     },
     dayBox: {
