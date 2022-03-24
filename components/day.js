98dd230e6e4304dd116e20d160572adbac753de8
Aaron Scofield <tuj83098@temple.edu>
2022-03-24 17:40:26 -0400

    get_links from api

--- components/day.js
+++ components/day.js
@@ -4,8 +4,25 @@ import Container from "@mui/material/Container";
 import styles from '../App.style';
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 export default function Day({parentToChild}){
+    
+    axios.get('http://54.226.36.70/api/get_links', {
+        params: {
+          username: "testUsername"
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+
     return(
         <div className="dayBox">
             <View style={styles.dayBox}>
