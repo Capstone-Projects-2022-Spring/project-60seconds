e48b0d1f0e76c8e36e3bda36b3b9c8f0cba376ec
Aaron Scofield <tuj83098@temple.edu>
2022-03-27 17:33:01 -0400

    day component displays link returned from api call

--- components/day.js
+++ components/day.js
@@ -3,32 +3,34 @@ import { View } from 'react-native';
 import styles from '../App.style';
 import axios from "axios";
 
+
 export default function Day({parentToChild}){
     
-    //querying backend for links based on day. to be implemented in new branch. 
+    let selectedDateString = parentToChild.getFullYear() + '-' + (parentToChild.getMonth()+1) + '-' + parentToChild.getDate();
+    //console.log(selectedDateString);
+    const [links, setLinks] = useState();
 
-    /*axios.get('http://54.226.36.70/api/get_links', {
+    //querying backend for links based on day.
+    axios.get('http://54.226.36.70/api/get_links', {
         params: {
           username: "testUsername",
-          date: '2022-03-24'
+          date: selectedDateString
         }
       })
       .then(function (response) {
-        let link = response.data[0].link
-        console.log(response.data[0].link);
+        //let link = response.data[0].link;
+        console.log(response.data);
+        setLinks(response.data[0].link);
       }) 
       .catch(function (error) {
         console.log(error);
-      })
-      .then(function () {
-        // always executed
       });
-      */
 
     return(
         <div className="dayBox">
             <View style={styles.dayBox}>
-                <p>{parentToChild.toString()}</p>
+                <h3>{parentToChild.toDateString()}</h3> 
+                <p>{links}</p>
             </View>
         </div>
     )
