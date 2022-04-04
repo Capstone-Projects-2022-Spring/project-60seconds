4d72859f53ae896bd81e26d2d6769ea7ae1fae70
Zack Waxler <tuj00149@temple.edu>
2022-04-03 21:06:53 -0700

    Implemented user specific recordings

--- components/day.js
+++ components/day.js
@@ -6,40 +6,45 @@ import PlayButton from './playbutton';
 
 axios.defaults.withCredentials = true;
 
-export default function Day({parentToChild}){
-    
+export default function Day({parentToChild}) {
+    let username;
+
     let selectedDateString = parentToChild.getFullYear() + '-' + (parentToChild.getMonth()+1) + '-' + parentToChild.getDate();
     //console.log(selectedDateString);
     const [data, setData] = useState('');
 
     const parentToChild2 = (linkToSet) => {
       setData(linkToSet.toString());
-  }
+    }
 
-    //querying backend for links based on day.
-    axios.get('https://api.60seconds.io/api/get_links', {
-        params: {
-          username: "testUsername",
-          date: selectedDateString
-        }
-      })
-      .then(function (response) {
-        //console.log(response.data);
-        if(response.data.length == 0){
-          parentToChild2('No recordings made this day.');
-        } else {
-          //for milestone demo 2, it plays the most recent recording returned from the query
-          parentToChild2(response.data[response.data.length - 1].link);
-        }
-      }) 
-      .catch(function (error) {
-        console.log(error);
-      });
+    axios.get('https://api.60seconds.io/api/user').then(function(response) {
+      username = response.data.username;
+
+      axios.get('https://api.60seconds.io/api/get_links', {
+          params: {
+            username: username,
+            date: selectedDateString
+          }
+        }).then(function (response) {
+          //console.log(response.data);
+          if(response.data.length == 0){
+            parentToChild2('No recordings made this day.');
+          } else {
+            //for milestone demo 2, it plays the most recent recording returned from the query
+            parentToChild2(response.data[response.data.length - 1].link);
+          }
+        }).catch(function (error) {
+          console.log(error);
+        });
+
+    });
+
+
 
     return(
         <div className="dayBox">
             <View style={styles.dayBox}>
-                <h3>{parentToChild.toDateString()}</h3> 
+                <h3>{parentToChild.toDateString()}</h3>
                 <PlayButton parentToChild2={data}/>
             </View>
         </div>
