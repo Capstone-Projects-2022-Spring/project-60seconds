c476df46b9217cf17f2fcc9fb67cae7b2182a17d
Sam Sepial <zprestoz@protonmail.com>
2022-04-10 02:34:22 -0400

    Fixed the cutting out when the user pauses. Next part is saving the string to the backend using an axios.post() command.

--- components/profile.js
+++ components/profile.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import ProfilePic from '../assets/zach.jpg'
+import ProfilePic from '../assets/stockProfilePicture.jpg'
 import "../styles/profile.css";
 
 export default function Profile() {
@@ -13,20 +13,27 @@ export default function Profile() {
 		}
 	}, []);
 
-
-
-
     return (
         <>
-			<div className="profile-card">
-				<img src={ProfilePic} className="profile-picture"/>
-				<h1 className="username">{userName}</h1>
-				<div className="about-me-card">
-					<p className="about-me-input">This is a test of the Profile Page,
-							making sure that everything looks nice and pretty</p>
+			<div className="page-layout">
+				<div className="profile-column">
+					<img src={ProfilePic} className="profile-picture"/>
+					<h1 className="username">Username:</h1>
+					<div className="profile-column-info">
+						<p className="first-name">First Name:</p>
+						<p className="last-name">Last Name:</p>
+						<p className="school">School:</p>
+						<p className="occupation">Occupation:</p>
+						<p className="basic-info-label">Basic Information</p>
+						<button className="edit-profile-button">Edit Page</button>
+					</div>
 				</div>
-				<p><button className="profile-page-button">View Recordings</button></p>
+				<div className="event-column">
+					<div className="event">
+						<p className="event-name">Event Name</p>
+					</div>
+				</div>
 			</div>
-        </>
+       </>
     )
 }
--- components/recorder.js
+++ components/recorder.js
@@ -15,7 +15,6 @@ export default function recorder() {
 
 	const [recording, setRecording] = React.useState();
 	const [recordings, setRecordings] = React.useState([]);
-	let isRecording = false;
 
 	async function startRecording() {
 		try {
@@ -31,8 +30,7 @@ export default function recorder() {
 			);
 			setRecording(recording);
 			console.log('Recording started');
-			isRecording = true;
-			startTranscribe();
+			transcription();
 		} catch (err) {
 			console.error('Failed to start recording', err);
 		}
@@ -92,7 +90,6 @@ export default function recorder() {
 
 
 		setRecordings(updatedRecordings);
-		isRecording = false;
 
 		let user = await axios.get('https://api.60seconds.io/api/user');
 		sendToServer(recording, user.data.username);
@@ -119,27 +116,31 @@ export default function recorder() {
 		});
 	}
 
-	function startTranscribe() {
-		var output = document.getElementById("content");
-		var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-		var recognition = new SpeechRecognition();
-		recognition.onstart = function() {
-			if (isRecording) {
-				console.log('is recording, start speech to text');
-			}
-		};
+	function transcription() {
+		var speech = true;
+		var transcriptFinal = '';
+		window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+		const recognition = new SpeechRecognition();
+		recognition.interimResults = true;
+		const words = document.querySelector('.words');
+		words.appendChild(content);
 
-		recognition.onspeechend = function() {
-			console.log('is not recording, end speech to text');
-			recognition.stop();
+		recognition.addEventListener('result', e => {
+			const transcript = Array.from(e.results)
+				.map(result => result[0])
+				.map(result => result.transcript)
+				.join('');
+
+			document.getElementById('content').innerHTML = transcript;
+			console.log(transcript);
+			transcriptFinal = transcriptFinal + transcript;
+		});
+
+		if (speech === true) {
+			recognition.start();
+			recognition.addEventListener('end', recognition.start);
 		}
-
-		recognition.onresult = function(event) {
-			var transcript = event.results[0][0].transcript;
-			output.innerHTML = transcript;
-			output.classList.remove("hide");
-		};
-		recognition.start();
+		console.log(transcriptFinal);
 	}
 
 	return (
